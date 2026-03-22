dfa13d4b9c9dc9b87be06e4700bdf3a35aad008b
Krish Naharki <krishnaharki827@gmail.com>
2026-03-22 20:43:48 +0545

    Add programs for Armstrong number, digit count, HCF, prime check, and vowel count

---
+++ Week1_Codes/armstrong.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number: "))
+temp = num
+power = len(str(num))
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum = sum + digit ** power
+    temp = temp // 10
+
+if sum == num:
+    print("Armstrong number")
+else:
+    print("Not an Armstrong number")
