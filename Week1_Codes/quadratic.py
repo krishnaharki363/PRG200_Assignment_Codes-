f4f9cad5d29ae8e73047db39fb407c874205ce1e
Krish Naharki <krishnaharki827@gmail.com>
2026-03-22 18:00:08 +0545

    Fix formatting issues and correct root calculation in quadratic equation program

--- Week1_Codes/quadratic.py
+++ Week1_Codes/quadratic.py
@@ -1,22 +1,27 @@
-#Write a program to find the roots of a quadratic equation. Check all conditions for real roots, equal roots 
-# and imaginary roots and display the roots. 
-#Formula = b*b - 4*a*c/ 
+import math
 
-import math 
-
-a = float(input("Enter the value of a: "))
-b = float(input("Enter the value of b: "))
-c = float(input("Enter the value of c: "))
+a = float(input("Enter value of a: "))
+b = float(input("Enter value of b: "))
+c = float(input("Enter value of c: "))
 
 d = b*b - 4*a*c
 
-if a ==0:
+if a == 0:
     print("It is not a quadratic equation.")
-if d > 0:
+elif d > 0:
     r1 = (-b + math.sqrt(d)) / (2*a)
-    r2 = (-b _ math.sqrt(d)) / (2*a)
+    r2 = (-b - math.sqrt(d)) / (2*a)
     print("Roots are real and different")
-    print("Roots 1 =", r1)
-    print("Root 2 =," r2)
+    print("Root 1 =", r1)
+    print("Root 2 =", r2)
 elif d == 0:
-    
+    r = -b / (2*a)
+    print("Roots are real and equal")
+    print("Root 1 =", r)
+    print("Root 2 =", r)
+else:
+    real = -b / (2*a)
+    imag = math.sqrt(-d) / (2*a)
+    print("Roots are imaginary")
+    print("Root 1 =", real, "+", imag, "i")
+    print("Root 2 =", real, "-", imag, "i")
