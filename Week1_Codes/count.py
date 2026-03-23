ad3358b81bdac101c6b6a3b8b387ef7b7436c142
Krish Naharki <krishnaharki827@gmail.com>
2026-03-23 21:59:06 +0545

    Improve comments for clarity in digit counting program

--- Week1_Codes/count.py
+++ Week1_Codes/count.py
@@ -2,7 +2,7 @@ num = int(input("Enter a large number: "))
 count = 0
 
 while num > 0:
-    num = num // 10
-    count += 1
+    num = num // 10 #removes last number 
+    count = count + 1 # each times digit is removed, count increases. 
 
 print("Number of digits =", count)
