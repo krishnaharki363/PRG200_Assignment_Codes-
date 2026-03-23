59b2f801cd928cb1f6970a88ff25e9abcbb11dc2
Krish Naharki <krishnaharki827@gmail.com>
2026-03-23 21:14:28 +0545

    Add detailed docstring explaining Armstrong numbers

--- Week1_Codes/armstrong.py
+++ Week1_Codes/armstrong.py
@@ -1,3 +1,10 @@
+"""
+An Armstrong number (also called a narcissistic number) is a number that is equal to the sum of its own digits each raised to the power of the number of digits.
+## Simple Definition
+If a number has n digits, then:
+Sum of (each digit ^ n) = the number itself
+"""
+
 num = int(input("Enter a number: "))
 temp = num
 power = len(str(num))
